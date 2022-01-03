7ccea1d76e4e9ca5b37852e06089c97e4505fab8
Khoironi Kurnia Syah <khoironidev@gmail.com>
2022-01-03 20:21:14 +0800

    feat: add seo

---
+++ next-seo.config.js
@@ -0,0 +1,27 @@
+export default {
+  title: "Coronavirus - Khoironi Kurnia Syah",
+  description:
+    "Track the corona virus over the world. Find the detail every country about the virus now.",
+  canonical: process.env.HOSTNAME,
+  noindex: false,
+  openGraph: {
+    url: process.env.HOSTNAME,
+    title: "Coronavirus - Khoironi Kurnia Syah",
+    description:
+      "Track the corona virus over the world. Find the detail every country about the virus now.",
+    images: [
+      {
+        url: "/images/corona-tracker.png",
+        width: 1920,
+        height: 1080,
+        alt: "Track the corona virus over the world.",
+      },
+    ],
+    site_name: "Coronavirus",
+  },
+  twitter: {
+    handle: "@zekhoi",
+    site: "@zekhoi",
+    cardType: "summary_large_image",
+  },
+};
