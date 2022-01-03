d4c8fafde4e700e46b67033ec9e37f4d4ee69000
Khoironi Kurnia Syah <khoironidev@gmail.com>
2022-01-03 16:21:25 +0800

    feat: add ionicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import "../styles/mapbox.css";
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        type="module"
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+        strategy="lazyOnload"
+      />
+      <Script
+        nomodule
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+        strategy="lazyOnload"
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
